12943f1f4d5510a261def23273e52492c318bd79
ElayGelbart <elaygelbart@gmail.com>
2021-11-29 12:51:38 +0200

    Basic Random Quote

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,3 +1,27 @@
+import { useEffect, useState } from "react"
+import QuoteJSON from "../assets/quotes.json"
 export default function App() {
-  return
+  const [QuoteText, setQuoteText] = useState(null);
+  const [QuoteAuthor, setQuoteAuthor] = useState(null);
+
+  useEffect(() => {
+    randomQuote()
+  }, [])
+
+  function randomQuote() {
+    const quoteOBJ = QuoteJSON.quotes;
+    const length = quoteOBJ.length;
+    const ranNumber = Math.floor(Math.random() * length);
+    console.log(ranNumber);
+    const selectedQuote = quoteOBJ[ranNumber];
+    setQuoteText(selectedQuote.quote)
+    setQuoteAuthor(selectedQuote.author)
+  }
+
+  return <div id="quote-box">
+    <div id="text">{QuoteText}</div>
+    <div id="author">{QuoteAuthor}</div>
+    <div id="new-quote" onClick={() => { randomQuote() }}>new</div>
+    <a id="tweet-quote" href="twitter.com/intent/tweet">Something</a>
+  </div>
 }
